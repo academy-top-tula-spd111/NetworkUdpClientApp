7950443935685df44de7ad496e6373cae6af31af
maxefimov <efimov.top.academy@gmail.com>
2023-06-17 17:42:50 +0300

    Добавьте файлы проекта.

---
+++ NetworkUdpClentSenderApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+using var clientUdp = new UdpClient();
+
+string message = "Hello world from UdpClient";
+byte[] buffer = Encoding.UTF8.GetBytes(message);
+IPEndPoint remoteEP = new IPEndPoint(IPAddress.Loopback, 7777);
+
+int bytes = await clientUdp.SendAsync(buffer, remoteEP);
+Console.WriteLine($"sends {bytes} bytes");
---
+++ NetworkUdpClientApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var serverUdp = new UdpClient(7777);
+Console.WriteLine("Server UDP start....");
+
+var result = await serverUdp.ReceiveAsync();
+Console.WriteLine(Encoding.UTF8.GetString(result.Buffer));
